8ecbe8ef7f16fb50d881ea3f015c1d7ca24c403c
rafael <skcc321@gmail.com>
2019-03-25 21:48:08 +0200

    [~] secter key base

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.secret_key_base = SecureRandom.hex(64)
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
