421937d25384f0ef3f0459f915cf0739f27861fa
skcc321 <skcc321@gmail.com>
2019-03-27 02:08:55 +0200

    [+] simplecov

--- deploy_pin.gemspec
+++ deploy_pin.gemspec
@@ -30,6 +30,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "ruby-progressbar", "~> 1.10"
   spec.add_dependency "rake", "~> 10.0"
 
+  spec.add_development_dependency 'simplecov'
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "pry", "~> 0.12"
   spec.add_development_dependency "minitest", "~> 5.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,10 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 require "rails/test_unit/reporter"
 Rails::TestUnitReporter.executable = 'bin/test'
 
+
+require 'simplecov'
+SimpleCov.start
+
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
