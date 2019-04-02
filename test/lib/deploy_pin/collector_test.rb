5077db0d0ac4fca672fa94272f19f7b9c90d8c81
skcc321 <skcc321@gmail.com>
2019-04-02 23:28:04 +0300

    [~] setup consts (test)

--- test/lib/deploy_pin/collector_test.rb
+++ test/lib/deploy_pin/collector_test.rb
@@ -7,31 +7,31 @@ class DeployPin::Collector::Test < ActiveSupport::TestCase
       groups ["I", "II", "III"]
       fallback_group "I"
     end
-  end
 
-  collector = DeployPin::Collector.new(groups: [DeployPin.fallback_group])
+    @collector = DeployPin::Collector.new(groups: [DeployPin.fallback_group])
+  end
 
   test "files" do
     assert_nothing_raised do
-      collector.files
+      @collector.files
     end
   end
 
   test "tasks" do
     assert_nothing_raised do
-      collector.tasks
+      @collector.tasks
     end
   end
 
   test "run" do
     assert_nothing_raised do
-      collector.run {|x| }
+      @collector.run {|x| }
     end
   end
 
   test "list" do
     assert_nothing_raised do
-      collector.list {|x| }
+      @collector.list {|x| }
     end
   end
 end
--- test/lib/deploy_pin/task_test.rb
+++ test/lib/deploy_pin/task_test.rb
@@ -7,55 +7,55 @@ class DeployPin::Task::Test < ActiveSupport::TestCase
       groups ["I", "II", "III"]
       fallback_group "I"
     end
-  end
 
-  task = DeployPin::Task.new('test/support/files/task.rb')
+    @task = DeployPin::Task.new('test/support/files/task.rb')
+  end
 
   test "run" do
     assert_nothing_raised do
-      task.run
+      @task.run
     end
   end
 
   test "mark" do
     assert_nothing_raised do
-      task.mark
+      @task.mark
     end
   end
 
   test "done?" do
     assert_nothing_raised do
-      task.done?
+      @task.done?
     end
   end
 
   test "parse_file" do
     assert_nothing_raised do
-      task.parse_file
+      @task.parse_file
     end
   end
 
   test "details" do
     assert_nothing_raised do
-      task.details
+      @task.details
     end
   end
 
   test "eql?" do
     assert_nothing_raised do
-      task.eql?(DeployPin::Task.new('test/support/files/task.rb'))
+      @task.eql?(DeployPin::Task.new('test/support/files/task.rb'))
     end
   end
 
   test "group_index" do
     assert_nothing_raised do
-      task.send(:group_index)
+      @task.send(:group_index)
     end
   end
 
   test "<=>" do
     assert_nothing_raised do
-      task.send(:<=>, DeployPin::Task.new('test/support/files/task.rb'))
+      @task.send(:<=>, DeployPin::Task.new('test/support/files/task.rb'))
     end
   end
 end
