b8eb3559393d9e4f3f88b73ce4ef85afa359255d
Viktor Sych <skcc321@gmail.com>
2023-02-16 01:20:14 +0200

    [~] rubocop autocorrection

--- app/models/deploy_pin/application_record.rb
+++ app/models/deploy_pin/application_record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class DeployPin::ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+module DeployPin
+  class ApplicationRecord < ActiveRecord::Base
+    self.abstract_class = true
+  end
 end
--- app/models/deploy_pin/record.rb
+++ app/models/deploy_pin/record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class DeployPin::Record < DeployPin::ApplicationRecord
-  self.table_name = 'deploy_pins'
+module DeployPin
+  class Record < DeployPin::ApplicationRecord
+    self.table_name = 'deploy_pins'
+  end
 end
