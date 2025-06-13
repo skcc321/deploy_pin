6e7034e4a307c73150de78cd0ed8096d035f8449
Serhii Bodnaruk <sergiwez@gmail.com>
2025-06-13 12:49:11 +0300

    [~] Raise an error if no group and fallback group provided

--- test/lib/generators/deploy_pin/task_generator_test.rb
+++ test/lib/generators/deploy_pin/task_generator_test.rb
@@ -8,21 +8,28 @@ class DeployPin::TaskGeneratorTest < Rails::Generators::TestCase
   destination Rails.root.join('tmp/generators')
   setup :prepare_destination
 
-  test 'generator runs without errors (without group)' do
-    assert_nothing_raised do
-      run_generator []
+  test 'raises error if group is missing' do
+    assert_raises(Thor::Error) do
+      DeployPin::TaskGenerator.new([], {}, destination_root: destination_root)
     end
   end
 
   test 'generator runs without errors (with group)' do
     assert_nothing_raised do
-      run_generator ['title -g I -a author']
+      DeployPin::TaskGenerator.new(
+        ['title'],
+        { group: 'I', author: 'author' },
+        destination_root: destination_root
+      )
     end
   end
 
   test 'generator runs without errors (with --parallel flag)' do
     assert_nothing_raised do
-      run_generator ['--parallel']
+      DeployPin::TaskGenerator.new(
+        ['title', '--parallel'],
+        { group: 'I', author: 'author' }
+      )
     end
   end
 end
